2c7cb99f84c1d6a67c8dcc054148a307c992351f
Pavel Ravits <vim55k@gmail.com>
2023-10-05 14:48:53 +0300

    add component

---
+++ components/how-to-pr/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export const HowToPr = () => {
+  return (
+    <Image
+      src="/how-to-pr/1-edit-this-page.png"
+      alt={"how-to-pr"}
+      height={100}
+      width={200}
+    />
+  );
+};
